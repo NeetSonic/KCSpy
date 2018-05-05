31b0c12478e68cc4fec5a2cd451101a65eacd61a
Neetsonic <Neetsonic@gmail.com>
2018-05-05 09:28:20 +0800

    dev

---
+++ KCSpy/View/FrmMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace KCSpy.View
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
